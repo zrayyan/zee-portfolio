9dfc2c0f38a0ff19fefc4af287b3615d7d5c2292
zrayyan <77869934+zrayyan@users.noreply.github.com>
2026-03-07 23:57:15 -0500

    replace lion loader with custom eagle SVG animation

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -19,14 +19,17 @@ export default function PageTransition({ children }: { children: ReactNode }) {
     <>
       {loading && (
         <div className="fixed inset-0 flex items-center justify-center bg-background z-50">
-          {/* running lion animation */}
-          <motion.div
-            className="text-6xl"
-            animate={{ x: [0, 30, 0] }}
-            transition={{ repeat: Infinity, duration: 0.8, ease: "linear" }}
+          {/* animated eagle sketch */}
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+            className="w-16 h-16 fill-primary"
+            animate={{ x: [0, 20, 0], rotate: [0, 10, 0] }}
+            transition={{ repeat: Infinity, duration: 1.2, ease: "easeInOut" }}
           >
-            🦁
-          </motion.div>
+            <path d="M10,50 C20,20 40,20 50,40 C60,20 80,20 90,50 C80,60 70,55 60,60 C50,65 40,60 30,65 C20,70 15,60 10,50 Z" />
+            <path d="M30,50 L40,40 L50,50 L60,40 L70,50" stroke="#fff" strokeWidth="2" fill="none" />
+          </motion.svg>
         </div>
       )}
       <AnimatePresence mode="wait">
