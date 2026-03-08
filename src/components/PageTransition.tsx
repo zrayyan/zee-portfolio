895d1c2f62d3d278ce471fe69604a8c6b3d1470f
zrayyan <77869934+zrayyan@users.noreply.github.com>
2026-03-08 00:04:08 -0500

    use eagle emoji for loader to match desired look

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -19,18 +19,14 @@ export default function PageTransition({ children }: { children: ReactNode }) {
     <>
       {loading && (
         <div className="fixed inset-0 flex items-center justify-center bg-background z-50">
-          {/* animated eagle sketch */}
-          <motion.svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 120 80"
-            className="w-16 h-16 fill-primary"
-            animate={{ x: [0, 20, 0], rotate: [0, 5, 0] }}
+          {/* emoji-style eagle loader */}
+          <motion.div
+            className="text-6xl"
+            animate={{ x: [0, 20, 0], rotate: [0, 10, 0] }}
             transition={{ repeat: Infinity, duration: 1.2, ease: "easeInOut" }}
           >
-            {/* simplified eagle silhouette */}
-            <path d="M5 40 Q30 20 55 40 T105 40 Q85 25 75 30 Q65 20 55 30 Q45 20 35 30 Q25 20 15 40 Z" />
-            <path d="M55 40 L55 60 L45 70 L60 70 L65 60 Z" fill="#fff" />
-          </motion.svg>
+            🦅
+          </motion.div>
         </div>
       )}
       <AnimatePresence mode="wait">
